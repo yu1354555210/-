96e832e83c5a369a6eef8e4aaacc4a2158c4d7eb
yu1354555210 <694960149@qq.com>
2018-10-11 17:55:37 +0800

    去掉些垃圾

--- 小程序-图文列表/utils/ald-stat-conf.js
+++
@@ -1,5 +0,0 @@
-exports.app_key = "35bd38e32bd403243e08d6f506a46019"; //请在此行填写从阿拉丁后台获取的appkey
-exports.getLocation = false; //默认不获取用户坐标位置
-exports.appid = "wx88196267ece1f564"; //用于用户登录、微信转发群信息、二维码等微信官方功能
-exports.appsecret = "eaac1befa7d77ce5e92571cdfc879a68";//用于用户登录、微信转发群信息、二维码等微信官方功能
-exports.defaultPath = 'pages/list/list';//小程序的默认首页, 用于分享时path为空时
