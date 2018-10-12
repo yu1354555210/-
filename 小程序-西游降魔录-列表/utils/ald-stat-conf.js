2b8d47d968c56b2b6dde78d6413133c2ad4d5e33
yu1354555210 <694960149@qq.com>
2018-10-12 17:02:35 +0800

    西游降魔录

---
+++ 小程序-西游降魔录-列表/utils/ald-stat-conf.js
@@ -0,0 +1,6 @@
+exports.app_key = "bd163efbc65a6dd27dd26ed3d9d84d3d"; //请在此行填写从阿拉丁后台获取的appkey
+exports.getLocation = false; //默认不获取用户坐标位置
+exports.appid = "wx9f19356e23468526"; //用于用户登录、微信转发群信息、二维码等微信官方功能
+exports.appsecret = "db9f2ee384398cb741181b1e457a50b9";//用于用户登录、微信转发群信息、二维码等微信官方功能
+exports.defaultPath = 'pages/list/list';//小程序的默认首页, 用于分享时path为空时
+
